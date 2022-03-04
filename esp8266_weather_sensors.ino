ff798f91026697bf6c33978a00307afcd198a05b
Evis Vasiu <evis.vasiu@gmail.com>
2022-03-04 01:58:14 +0100

    MQTT included

--- esp8266_weather_sensors.ino
+++ esp8266_weather_sensors.ino
@@ -8,40 +8,35 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-unsigned long delayTime;
-bool status;
+#define LED 2     //onboard LED
+#define SEALEVELPRESSURE_HPA (1013.25)  //Sea level constant
 
 // Wifi credentials
 const char* ssid = "KabelBox-1B6C";
 const char* password = "80299875789367562743";
 
-// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+//MQTT broker
 const char* mqtt_server = "138.3.246.220";
 
-// Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
+// MQTT client initialization
 WiFiClient espClient;
 PubSubClient client(espClient);
 #define MQTTQOS1
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-
-SHT31 sht;
+Adafruit_BME280 bme;  //I2C BME
+SHT31 sht;            //I2C sht
 
 // GPIO where the DS18B20 is connected to
-const int oneWireBus = 2; 
+const int oneWireBus = 0; 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(oneWireBus);
 // Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
-int sensorValue = 0;  // value read from the pot
+int sensorValue = 0;         // value read from the pot
 
-// Don't change the function below. This functions connects your ESP8266 to your router
+//wifi initialization
 void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
@@ -62,24 +57,13 @@ void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    /*
-     YOU MIGHT NEED TO CHANGE THIS LINE, IF YOU'RE HAVING PROBLEMS WITH MQTT MULTIPLE CONNECTIONS
-     To change the ESP device ID, you will have to give a new name to the ESP8266.
-     Here's how it looks:
-       if (client.connect("ESP8266Client")) {
-     You can do it like this:
-       if (client.connect("ESP1_Office")) {
-     Then, for the other ESP:
-       if (client.connect("ESP2_Garage")) {
-      That should solve your MQTT multiple connections problem
-    */
     if (client.connect("ESP8266Client", "jezerca", "Password@2")) {
       Serial.println("connected");  
       // Subscribe or resubscribe to a topic
       // You can subscribe to more topics (to control more LEDs in this example)
       //client.subscribe("room/lamp");
-    } else {
+    } 
+    else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -90,30 +74,25 @@ void reconnect() {
 }
 
 void setup() {
-  Serial.begin(9600);
-  
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  client.setServer(mqtt_server, 1883); 
   Wire.begin();
   Serial.println(F("BME280 test"));
-
-  status = bme.begin(0x76);   //bme I2C Address
-  
+  bool status = bme.begin(0x76);   //bme I2C Address
   sht.begin(0x44);            //sht30 I2C Address  
   
   // Start the DS18B20 sensor
   sensors.begin();
-  
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
   }
-
   Serial.println("-- Default Test --");
-  delayTime = 5000;
+  unsigned long delayTime = 5000;
 
-
+  //sht
   Wire.setClock(100000);
   uint16_t stat = sht.readStatus();
   Serial.print(stat, HEX);
@@ -121,7 +100,6 @@ void setup() {
 }
 
 void loop() { 
-  
   if (!client.connected()) {
     reconnect();
   }
@@ -131,23 +109,28 @@ void loop() {
   //ShT30 sensor
   sht.read();
   delay(2000);
+  float sht_t = sht.getTemperature();
+  float sht_h = sht.getHumidity();
+  digitalWrite(LED, LOW);
   Serial.print("SHT30 Temperature: ");
-  Serial.print(sht.getTemperature(), 1);
+  Serial.print(sht_t, 1);
   Serial.println("ºC");
   Serial.print("SHT30 Humidity: ");
-  Serial.print(sht.getHumidity(), 1);
+  Serial.print(sht_h, 1);
   Serial.print("%");
   Serial.println("\n");
+  client.publish("esp8266/sht_t", String(sht_t).c_str());
+  client.publish("esp8266/sht_h", String(sht_h).c_str());
   delay(1000);
   
   //DS18b20 sensor
   sensors.requestTemperatures(); 
   float temperatureC = sensors.getTempCByIndex(0);
-  float temperatureF = sensors.getTempFByIndex(0);
   Serial.print("DS18B20: ");
   Serial.print(temperatureC);
   Serial.print("ºC");
   Serial.println("\n");
+  client.publish("esp8266/ds18b20", String(temperatureC).c_str());
   delay(1000);
   
   // UV sensor
@@ -156,25 +139,33 @@ void loop() {
   Serial.print(sensorValue);
   Serial.print("mV");
   Serial.println("\n");
+  client.publish("esp8266/uv", String(sensorValue).c_str());
   delay(1000);
 
+  float bme_t = bme.readTemperature();
   Serial.print("BME Temperature: ");
-  Serial.print(bme.readTemperature());
+  Serial.print(bme_t);
   Serial.println("*C");
-  
+  client.publish("esp8266/bme_t", String(bme_t).c_str());
+
+  float bme_p = bme.readPressure() / 100.0;
   Serial.print("BME Pressure: ");
-  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print(bme_p);
   Serial.println("hPa");
-
+  client.publish("esp8266/bme_p", String(bme_p).c_str());
+  
+  float bme_a = bme.readAltitude(SEALEVELPRESSURE_HPA);
   Serial.print("BME Approx. Altitude: ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.print(bme_a);
   Serial.println("m");
+  client.publish("esp8266/bme_a", String(bme_a).c_str());
 
+  float bme_h = bme.readHumidity();
   Serial.print("BME Humidity: ");
-  Serial.print(bme.readHumidity());
+  Serial.print(bme_h);
   Serial.print("%");
   Serial.println("\n");
-
-  client.publish("test", "prova123");
+  digitalWrite(LED, HIGH);
+  client.publish("esp8266/bme_h", String(bme_h).c_str());
 
 }
